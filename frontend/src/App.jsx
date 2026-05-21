0ee84e127c38fb81a454963dffd3f1ab7042c6d2
JoFaTech2508 <johfaa123@gmail.com>
2026-05-22 00:02:13 +0200

    refactor: update App.jsx imports to @ alias

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-import AuthProvider from "./providers/AuthProvider";
-import Layout from "./layouts/Layout";
-import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import CreateGamePage from "./pages/CreateGamePage/CreateGamePage";
-import LobbyPage from "./pages/LobbyPage/LobbyPage";
-import UserProfilePage from "./pages/UserProfilePage/UserProfilePage";
-import UserGamesPage from "./pages/UserGamesPage/UserGamesPage";
-import GamePage from "./pages/GamePage/GamePage";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import AboutSpanishDicePage from "./pages/AboutSpanishDicePage/AboutSpanishDicePage";
+import AuthProvider from "@/providers/AuthProvider";
+import Layout from "@/layouts/Layout";
+import HomePage from "@/pages/HomePage/HomePage";
+import LoginPage from "@/pages/LoginPage/LoginPage";
+import RegisterPage from "@/pages/RegisterPage/RegisterPage";
+import CreateGamePage from "@/pages/CreateGamePage/CreateGamePage";
+import LobbyPage from "@/pages/LobbyPage/LobbyPage";
+import UserProfilePage from "@/pages/UserProfilePage/UserProfilePage";
+import UserGamesPage from "@/pages/UserGamesPage/UserGamesPage";
+import GamePage from "@/pages/GamePage/GamePage";
+import AboutPage from "@/pages/AboutPage/AboutPage";
+import AboutSpanishDicePage from "@/pages/AboutSpanishDicePage/AboutSpanishDicePage";
+import TermsPage from "@/pages/TermsPage/TermsPage";
+import PrivacyPage from "@/pages/PrivacyPage/PrivacyPage";
+import AppearanceProvider from "@/providers/AppearanceProvider";
+import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
 import "./App.css";
-import TermsPage from "./pages/TermsPage/TermsPage";
-import PrivacyPage from "./pages/PrivacyPage/PrivacyPage";
-import AppearanceProvider from "./providers/AppearanceProvider";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
@@ -36,8 +36,8 @@ function App() {
               <Route path="/about-spanish-dice" element={<AboutSpanishDicePage />} />
               <Route path="/terms" element={<TermsPage />} />
               <Route path="/policy" element={<PrivacyPage />} />
-              <Route path="*" element={<NotFoundPage />} />
             </Route>
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </AppearanceProvider>
       </AuthProvider>
