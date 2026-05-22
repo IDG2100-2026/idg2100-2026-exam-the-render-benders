66d4071db082d8d246fa44a69d2cc46866ff47bd
sebamau <sebamau@stud.ntnu.no>
2026-05-22 14:36:10 +0200

    feat: auth router

---
+++ backend/routers/auth.router.js
@@ -0,0 +1,15 @@
+import express from "express";
+import authController from "../controllers/auth.controller.js";
+import { validateCreateUser, handleValidationErrors } from "../validators/user.validator.js";
+
+const authRouter = express.Router();
+
+authRouter.post("/register", validateCreateUser, handleValidationErrors, authController.register);
+authRouter.post("/login", authController.login);
+authRouter.post("/refresh", authController.refresh);
+authRouter.post("/logout", authController.logout);
+authRouter.post("/verify-email", authController.verifyEmail);
+authRouter.post("/resend-verification", authController.resendVerification);
+
+
+export default authRouter;
