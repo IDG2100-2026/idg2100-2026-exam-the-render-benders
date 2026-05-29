4346b8cc8b7c2a3edb94c71a55de825247a27af0
sebamau <sebamau@stud.ntnu.no>
2026-05-29 17:06:17 +0200

    fix: use env frontend url for cors

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ const limiter = rateLimit({
 
 // Allow requests from frontend (CORS)
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true
 }));
 
